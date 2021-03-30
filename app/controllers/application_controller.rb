f77e798357efc5b329476e3a1ee5efb534b0d4d2
Mohammed Aasif <mohammedaasif@tecnovators.in>
2021-03-30 13:18:30 +0530

    add developer and employer t user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :developer, :employer])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :developer, :employer])
   end
 end
